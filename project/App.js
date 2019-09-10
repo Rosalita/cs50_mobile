fd8f76f4f18047c6cf283d46b9928cf657bea1f8
Rosie Hamilton <rosie.hamilton@thingco.com>
2019-09-10 15:37:58 +0100

    wip

--- project/App.js
+++ project/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { Count } from './Count.js'
 
 export default class App extends React.Component {
 
