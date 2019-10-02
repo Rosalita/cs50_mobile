cc202df5503095bcc6ab69a379a6da058f15c095
Rosie <enchantrix@gmail.com>
2019-10-02 17:08:49 +0100

    work in progress

--- project1/App.js
+++ project1/App.js
@@ -3,55 +3,53 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
 
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
     this.state = {
+      mode: "work",
+      count: 1500,
+    }
+  }
 
+  componentDidMount(){
+    setInterval(this.decrementCount, 1000)
+  }
+
+  decrementCount = () => {
+    this.setState(prevState => ({count: prevState.count -1}))
+  }
+
+  toggleMode = () => {
+    if (this.state.mode === "work"){
+      this.setState(prevState => ({mode: "rest"}))
+    } else {
+      this.setState(prevState => ({mode: "work"}))
     }
   }
 
   render(){
     return (
-      <View style={styles.container}>
-        <Text>Pomodoro Timer</Text>
-  
-        <Timer />
-  
-        <Button title="Start" />
-        <Button title="Stop" />
-        <Button title="Pause" />
+      <View style={styles.appContainer}>
+        <Text style={styles.titleText}>Pomodoro Timer</Text>
+        <Text> Current mode : {this.state.mode}</Text>
+        <Text style={styles.count}>{this.state.count}</Text>
+        <Button title="test - change mode" onPress={() => this.toggleMode()}/>
       </View>
     );
   }
-  
 }
 
-class Timer extends React.Component {
-
-  constructor(){
-    super()
-    this.state = {
-      count: 0,
-    }
-  }
-  render() {
-    return (
-      <View>
-        <Text style={styles.timer}>I'm a timer: {this.state.count}</Text>
-
-      </View>
-    )
-  }
-}
 const styles = StyleSheet.create({
-  container: {
+  titleText:{
+      fontSize: 24,
+  },
+  appContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-
-  timer: {
-    fontSize: 30,
-  }
+  count: {
+    fontSize: 48,
+  },
 });
